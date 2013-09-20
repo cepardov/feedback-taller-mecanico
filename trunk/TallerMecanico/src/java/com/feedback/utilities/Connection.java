4e3b7bd808098e54d636304bec38d5a1f67104a4
pablo.santana.r@gmail.com <pablo.santana.r@gmail.com@113fcf30-f6c2-3366-6336-b17f46f7ae13>
2013-09-20 22:15:38 +0000

    nueva classe conexion "connection.java" en paquete: "com.feedback.utilities"

---
+++ trunk/TallerMecanico/src/java/com/feedback/utilities/Connection.java
@@ -0,0 +1,43 @@
+package com.feedback.utilities;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connection {
+     private static java.sql.Connection conn;
+    private static String login ="root";
+    private static String password ="";
+    private static String url = "jdbc:mysql://localhost:3306/tallerm";
+
+    public static java.sql.Connection getInstanceConnection() {
+        if (!(conn instanceof java.sql.Connection)) {
+            System.out.println("Conectando a la BD.");
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                conn = DriverManager.getConnection(url, login, password);
+            } catch (ClassNotFoundException ex) {
+                System.out.println(ex.getMessage());
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println(conn);
+        return conn;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (conn instanceof java.sql.Connection) {
+                conn.close();
+                conn = null;
+                System.out.println("Se ha cerrado la conexi�n de BD con exito.");
+            }
+
+        } catch (SQLException se) {
+            System.out.println(se.toString());
+            System.err.println("Se ha producido un error al cerrar la conexi�n de BD.");
+            System.err.println(se.getMessage());
+        }
+    }
+}
