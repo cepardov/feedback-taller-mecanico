03f66a0050c9bd83812bb844b23e286e090e31f8
atoncaster@gmail.com <atoncaster@gmail.com@113fcf30-f6c2-3366-6336-b17f46f7ae13>
2013-09-21 00:10:00 +0000

    trabajando con clienteBEANs

--- trunk/TallerMecanico/src/java/models/dao/clientedao.java
+++ trunk/TallerMecanico/src/java/models/dao/clientedao.java
@@ -1,13 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package models.dao;
 
-/**
- *
- * @author Luis
- */
 public class clientedao {
     
 }
