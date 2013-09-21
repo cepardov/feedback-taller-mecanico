b97c2999e4564e4048b14ce41b7132c70363208a
cepardov@gmail.com <cepardov@gmail.com@113fcf30-f6c2-3366-6336-b17f46f7ae13>
2013-09-21 02:32:31 +0000

    Reestructura se añadieron carpeta com

---
+++ trunk/TallerMecanico/src/java/com/models/beans/Clientebeans.java
@@ -0,0 +1,37 @@
+package com.models.beans;
+
+import java.util.List;
+import com.models.dao.Clientedao;
+import com.models.entity.Cliente;
+
+public class Clientebeans extends Cliente {
+    
+     private Clientedao clienteDao = new Clientedao();
+
+    
+    public List<Cliente> findPorNombre() {
+        return clienteDao.findPorNombre(nombre);
+    }
+    
+    public List<Cliente> findAll() {
+        return clienteDao.findAll();
+    }
+
+    public Cliente findByRut() {
+        Cliente cliente = null;
+        if (rut != null) {
+            cliente = clienteDao.findByRut(rut);
+        } else {
+            cliente = new Cliente();
+        }
+        return cliente;
+    }
+
+    public void save() {
+        clienteDao.save(this);
+    }
+
+    public void delete() {
+        clienteDao.delete(this);
+    }
+}
