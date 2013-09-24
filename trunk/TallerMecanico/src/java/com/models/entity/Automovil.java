18007fc006fd7cf2b73d4219df45c0ef33ba355b
pofo99@gmail.com <pofo99@gmail.com@113fcf30-f6c2-3366-6336-b17f46f7ae13>
2013-09-24 22:31:46 +0000

    clase entidad automovil

--- trunk/TallerMecanico/src/java/com/models/entity/Automovil.java
+++ trunk/TallerMecanico/src/java/com/models/entity/Automovil.java
@@ -9,5 +9,69 @@ package com.models.entity;
  * @author Luis
  */
 public class Automovil {
+    protected String patente;
+    protected String rutcliente;
+    protected String color;
+    protected int idmarca;
+    protected int idmodelo;
+    protected int año;
+    protected int cilindrada;
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public String getRutcliente() {
+        return rutcliente;
+    }
+
+    public void setRutcliente(String rutcliente) {
+        this.rutcliente = rutcliente;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getIdmarca() {
+        return idmarca;
+    }
+
+    public void setIdmarca(int idmarca) {
+        this.idmarca = idmarca;
+    }
+
+    public int getIdmodelo() {
+        return idmodelo;
+    }
+
+    public void setIdmodelo(int idmodelo) {
+        this.idmodelo = idmodelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+    
     
 }
